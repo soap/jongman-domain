8a9e708bf69e243072966d9465e0ed79b3d5ca1d
Prasit Gebsaap <prasit.gebsaap@outlook.com>
2024-08-22 20:29:23 +0700

    Add ResourceServiceInterface

--- src/Services/ScheduleService.php
+++ src/Services/ScheduleService.php
@@ -5,15 +5,17 @@ namespace Soap\Jongman\Core\Services;
 use App\Repositories\ScheduleRepository;
 use Soap\Jongman\Core\Application\User\User;
 use Soap\Jongman\Core\Factories\DailyLayoutFactory;
+use Soap\Jongman\Core\Interfaces\DailyLayoutFactoryInterface;
 use Soap\Jongman\Core\Interfaces\LayoutFactoryInterface;
+use Soap\Jongman\Core\Interfaces\ScheduleRepositoryInterface;
 use Soap\Jongman\Core\Interfaces\ScheduleServiceInterface;
 
 class ScheduleService implements ScheduleServiceInterface
 {
     public function __construct(
-        private ScheduleRepository $scheduleRepository,
-        private ResourceService $resourceService,
-        private DailyLayoutFactory $dailyLayoutFactory
+        private ScheduleRepositoryInterface $scheduleRepository,
+        private ResourceServiceInterface $resourceService,
+        private DailyLayoutFactoryInterface $dailyLayoutFactory
     ) {}
 
     public function getAll($includeInaccessible, User $user)
